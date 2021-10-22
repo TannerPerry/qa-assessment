2fe4f434ef7e6591e692a944358dd29c8f673062
Tanner Perry <tc-boy@hotmail.com>
2021-10-22 11:57:36 -0600

    first commit

---
+++ tictactoe.test.ts
@@ -0,0 +1,48 @@
+import { Builder, Capabilities, By } from "selenium-webdriver"
+
+const chromedriver = require('chromedriver')
+
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+
+beforeAll(async () => {
+    await driver.get('http://127.0.0.1:5500/tictacjs.html')
+})
+
+afterAll(async () => {
+    await driver.quit()
+})
+
+test('I can start a game', async () => {
+
+    let button = await (await driver).findElement(By.id('start-game'));
+    await button.click();
+
+});
+it('I make a move', async () => {
+
+    let makeMove = await(await driver).findElement(By.xpath('//*[@id="cell-0"]'))
+    await makeMove.click()
+    await driver.sleep(2000)
+})
+it('I make a second move', async () => {
+
+    let makeMove = await(await driver).findElement(By.xpath('//*[@id="cell-4"]'))
+    await makeMove.click()
+    await driver.sleep(2000)
+})
+it('I make win a game', async () => {
+
+    let makeMove = await(await driver).findElement(By.xpath('//*[@id="cell-8"]'))
+    await makeMove.click()
+    await driver.sleep(2000)
+})
+it('I can restart a game', async () => {
+
+    driver.get("http://127.0.0.1:5500/tictacjs.html");
+    driver.navigate().refresh();
+    await driver.sleep(2000)
+    
+});
+
+
